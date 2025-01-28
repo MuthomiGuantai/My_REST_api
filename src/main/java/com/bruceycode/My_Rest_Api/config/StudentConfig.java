75f59effb46939914f0ed6891510343ebcd7c8ca
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-01-28 10:55:03 +0300

    connected to mysql db

--- src/main/java/com/bruceycode/My_Rest_Api/config/StudentConfig.java
+++ src/main/java/com/bruceycode/My_Rest_Api/config/StudentConfig.java
@@ -2,6 +2,7 @@ package com.bruceycode.My_Rest_Api.config;
 
 import com.bruceycode.My_Rest_Api.Repository.StudentRepository;
 import com.bruceycode.My_Rest_Api.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,18 +12,43 @@ import java.time.Month;
 import java.util.List;
 
 @Configuration
-public class StudentConfig {
+public class StudentConfig implements CommandLineRunner  {
+    @Autowired
+    StudentRepository repository;
 
-    CommandLineRunner commandLineRunner(
-            StudentRepository repository
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("about to insert data");
+
+        Student victor = new Student(
+
+              //  2L,
+                "Victor",
+                "victor@gmail.com",
+                LocalDate.of(2002, Month.MARCH,5),
+                22
+        );
+        Student bruce = new Student(
+
+              //  1L,
+                "Bruce",
+                "bruce@gmail.com",
+                LocalDate.of(1997, Month.DECEMBER,5),
+                30
+        );
+        repository.saveAll(List.of(bruce,victor));
+    }
+  /*  CommandLineRunner commandLineRunner(
+
     ){
+        System.out.println("about to insert data");
         return args -> {
           Student bruce = new Student(
 
                             1L,
                             "Bruce",
                             "bruce@gmail.com",
-                            LocalDate.of(1994, Month.JANUARY,5),
+                            LocalDate.of(1997, Month.DECEMBER,5),
                             30
             );
             Student victor = new Student(
@@ -39,6 +65,6 @@ public class StudentConfig {
             );
 
         };
-    }
+    }*/
 
 }
