6ec403cff4cccf34319932ba207cad62d6653192
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-02-06 14:10:29 +0300

    code clean up

--- src/main/java/com/bruceycode/My_Rest_Api/config/StudentConfig.java
+++ src/main/java/com/bruceycode/My_Rest_Api/config/StudentConfig.java
@@ -55,33 +55,4 @@ public class StudentConfig implements CommandLineRunner {
 
 
 
-  /*  CommandLineRunner commandLineRunner(
-
-    ){
-        System.out.println("about to insert data");
-        return args -> {
-          Student bruce = new Student(
-
-                            1L,
-                            "Bruce",
-                            "bruce@gmail.com",
-                            LocalDate.of(1997, Month.DECEMBER,5),
-                            30
-            );
-            Student victor = new Student(
-
-                    2L,
-                    "Victor",
-                    "victor@gmail.com",
-                    LocalDate.of(2002, Month.MARCH,5),
-                    22
-            );
-
-            repository.saveAll(
-                    List.of(bruce, victor)
-            );
-
-        };
-    }*/
-
 
