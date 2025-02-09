cd55b0d5b820d83bdfbc1fe094eab3ed02a57722
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-02-09 17:41:41 +0300

    add student successfully test passed

--- src/test/java/com/bruceycode/My_Rest_Api/Service/StudentServiceTest.java
+++ src/test/java/com/bruceycode/My_Rest_Api/Service/StudentServiceTest.java
@@ -4,20 +4,20 @@ import com.bruceycode.My_Rest_Api.model.Student;
 import com.bruceycode.My_Rest_Api.service.StudentService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,17 +35,33 @@ public class StudentServiceTest {
     void setUp() {
         student = new Student();
         student.setId(1L);
-        student.setName("John Doe");
-        student.setEmail("john.doe@example.com");
+        student.setName("Bruce Muthomi");
+        student.setEmail("bruce.muthomi@gmail.com");
     }
 
     @Test
-    void canGetStudents() {
+    void canGetStudentsSuccessfully() {
         // when
         studentService.getStudents();
 
         // then
         verify(studentRepository).findAll();
+    }
+
+    @Test
+    void canAddNewStudentSuccessfully() {
+        // given
+        given(studentRepository.findStudentByEmail(student.getEmail()))
+                .willReturn(Optional.empty());
+
+        // when
+        studentService.addNewStudent(student);
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
 
