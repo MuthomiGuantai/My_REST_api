8bce5827197dd86bdaa9e6871248437880775a03
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-02-09 16:52:17 +0300

    Test get all students passed

--- src/test/java/com/bruceycode/My_Rest_Api/Service/StudentServiceTest.java
+++ src/test/java/com/bruceycode/My_Rest_Api/Service/StudentServiceTest.java
@@ -1,6 +1,4 @@
 package com.bruceycode.My_Rest_Api.Service;
-
-
 import com.bruceycode.My_Rest_Api.Repository.StudentRepository;
 import com.bruceycode.My_Rest_Api.model.Student;
 import com.bruceycode.My_Rest_Api.service.StudentService;
@@ -8,19 +6,47 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StudentServiceTest {
 
     @Mock
-    StudentRepository studentRepository;
+    private StudentRepository studentRepository;
+
     @InjectMocks
-    StudentService studentService;
-    private static Student student = null;
+    private StudentService studentService;
+
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        student = new Student();
+        student.setId(1L);
+        student.setName("John Doe");
+        student.setEmail("john.doe@example.com");
+    }
+
+    @Test
+    void canGetStudents() {
+        // when
+        studentService.getStudents();
+
+        // then
+        verify(studentRepository).findAll();
+    }
+
+
 }
